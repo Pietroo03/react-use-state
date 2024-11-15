947ba05ae1cf97d4555a28c35805d2867261451c
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-15 14:53:43 +0100

    add event to AppMain component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import AppHeader from './components/AppHeader'
+import AppMain from './components/AppMain'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
 
       <AppHeader />
+      <AppMain />
 
     </>
   )
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -4,4 +4,43 @@ export default function AppMain() {
 
     const [active, setActive] = useState(0)
 
+    function handleClick(e) {
+        const newActive = e.target.getAttribute('data-index')
+        console.log(newActive);
+
+        setActive(newActive)
+
+    }
+
+    return (
+
+        <>
+            <main>
+
+                <div className="container">
+                    <div className="languages">
+                        {languages.map((item, index) => (
+
+                            <div className="language-buttons" key={item.id}>
+                                <button onClick={handleClick} data-index={index} className="language">{item.title}</button>
+                            </div>
+
+                        ))}
+
+                    </div>
+                    <div className="descriptions">
+                        {languages.map((item, index) => (
+                            <div className="button-description">
+                                <div className={active == index ? 'active' : 'hide'}>{item.description}</div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+            </main>
+
+        </>
+
+    )
+
 }
