ef867dd68b262ece05929fa205a28936bf6388d7
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-18 10:15:31 +0100

    add changes

--- src/components/Button/Buttons.jsx
+++ src/components/Button/Buttons.jsx
@@ -1,11 +1,11 @@
 export default function Buttons({ languages, active, onChange }) {
 
     return (
-        <div className="languages">
+        <div className="languages-buttons">
             {languages.map((item, index) => (
 
-                <div className="language-buttons" key={item.id}>
-                    <button onClick={() => onChange(index)} data-index={index} className="language" aria-pressed={active == index}>{item.title}</button>
+                <div className="button" key={item.id}>
+                    <button onClick={() => onChange(index)} data-index={index} className="buttons" aria-pressed={active == index}>{item.title}</button>
                 </div>
 
             ))}
